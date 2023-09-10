c2355f438f5a782c870b00d4c7d49491b0220c57
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-09-10 18:03:33 +1000

    Update ubuntu-64 installer

--- install-ubuntu-64.sh
+++ install-ubuntu-64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 
+#
 . /lib/lsb/init-functions
 
 daemonname="deskpi"
@@ -10,7 +10,7 @@ safeshutdaemon=/lib/systemd/system/$daemonname-safeshut.service
 installationfolder=/home/$SUDO_USER/deskpi
 
 # install DeskPi stuff.
-log_action_msg "DeskPi Fan control script installation Start." 
+log_action_msg "DeskPi Fan control script installation Start."
 
 # Create service file on system.
 if [ -e $deskpidaemon ]; then
@@ -18,13 +18,13 @@ if [ -e $deskpidaemon ]; then
 fi
 
 # adding dtoverlay to enable dwc2 on host mode.
-sudo sed -i '/dtoverlay=dwc2*/d' /boot/firmware/config.txt 
-sudo sed -i '$a\dtoverlay=dwc2,dr_mode=host' /boot/firmware/config.txt 
+sudo sed -i '/dtoverlay=dwc2*/d' /boot/firmware/config.txt
+sudo sed -i '$a\dtoverlay=dwc2,dr_mode=host' /boot/firmware/config.txt
 
 # install PWM fan control daemon.
 log_action_msg "DeskPi main control service loaded."
 sudo apt-get install -qy gcc
-cd $installationfolder/drivers/c/ 
+cd $installationfolder/drivers/c/
 mv $installationfolder/drivers/c/pwmFanControl $installationfolder/drivers/c/pwmFanControl.old 2>/dev/null
 gcc -o $installationfolder/drivers/c/pwmFanControl $installationfolder/drivers/c/pwmControlFan.c
 gcc -o $installationfolder/drivers/c/fanStop $installationfolder/drivers/c/fanStop.c
@@ -36,6 +36,9 @@ sudo chmod 755 /usr/bin/pwmFanControl
 sudo chmod 755 /usr/bin/fanStop
 sudo chmod 755 /usr/bin/deskpi-config
 sudo chmod 755 /usr/bin/Deskpi-uninstall
+
+# Install 20-serial.rules
+echo "SUBSYSTEM==\"tty\",ATTRS{idVendor}==\"1a86\",ATTRS{idProduct}==\"7523\",SYMLINK+=\"ttyFAN0\"" | sudo tee /etc/udev/rules.d/20-serial.rules
 
 # Build Fan Daemon
 echo "[Unit]" > $deskpidaemon
@@ -62,24 +65,24 @@ echo "TimeoutSec=1" >> $safeshutdaemon
 echo "[Install]" >> $safeshutdaemon
 echo "WantedBy=halt.target shutdown.target poweroff.target" >> $safeshutdaemon
 
-log_action_msg "DeskPi Service configuration finished." 
+log_action_msg "DeskPi Service configuration finished."
 sudo chown root:root $safeshutdaemon
 sudo chmod 755 $safeshutdaemon
 
 sudo chown root:root $deskpidaemon
 sudo chmod 755 $deskpidaemon
 
-log_action_msg "DeskPi Service Load module." 
+log_action_msg "DeskPi Service Load module."
 sudo systemctl daemon-reload
 sudo systemctl enable $daemonname.service
 sudo systemctl start $daemonname.service &
 sudo systemctl enable $daemonname-safeshut.service
 
-# Finished 
-log_success_msg "DeskPi PWM Fan Control and Safeshut Service installed successfully." 
+# Finished
+log_success_msg "DeskPi PWM Fan Control and Safeshut Service installed successfully."
 # greetings and require rebooting system to take effect.
-log_action_msg "System will reboot in 5 seconds to take effect." 
+log_action_msg "System will reboot in 5 seconds to take effect."
 sudo sync
-sleep 5 
+sleep 5
 # sudo reboot
 echo "Reboot system for changes to take effect"
